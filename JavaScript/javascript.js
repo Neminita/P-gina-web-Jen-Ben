3ef6cecd43f253e1496035a8e4a8f15ede89f73b
BenjaCona <be.cona@duocuc.cl>
2024-05-07 05:04:55 -0400

    Avance con errores js

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,15 +1,17 @@
-const direccion = document.getElementById("direccion")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const ciudad = document.getElementById("ciudad")
-const cpostal = document.getElementById("cpostal")
-const form = document.getElementById("form")
-const parrafo = document.getElementById("warnings")
-
-form.addEventListener("submit",e=>{
-    e.preventDefault()
-    let warnings = ""
-    if(cpo.Value.length <6){
-        warnings+= 'El nombre no es valido <br>'
-    }
+document.addEventListener("DOMContentLoaded", () => {
+   fetchData() 
 })
+
+const fetchData = async ()=> {
+    try{
+        const res = await fetch('api.json')
+        const data = await res.json()
+        
+        detectarBotones(data)
+    } catch (error){
+        
+    }
+    }
+var buttonComprar = document.getElementById
+    botones.addEventListener('click')
+    
