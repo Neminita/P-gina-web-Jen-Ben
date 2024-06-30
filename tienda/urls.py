d194f47473e684325573e7209a86525a096f2c7a
BenjaCona <be.cona@duocuc.cl>
2024-06-30 17:32:31 -0400

    Incorporacion de DJANGO

---
+++ tienda/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .  import views
+
+urlpatterns = [
+    path('',views.home, name='inicio' ),
+    path('Carrito/',views.carrito, name='Carrito' ),
+    path('Clasica/',views.clasica, name='Clasica' ),
+    path('Contacto/',views.contacto, name='Contacto' ),
+    path('Inicio-Sesion/',views.inisesion, name='Iniciosesion' ),
+    path('Jazz/',views.jazz, name='Jazz' ),
+    path('Nosotros/',views.nosotros, name='Nosotros' ),
+    path('Registro/',views.registro, name='Registro' ),
+    path('Rock/',views.rock, name='Rock' ),
+]
