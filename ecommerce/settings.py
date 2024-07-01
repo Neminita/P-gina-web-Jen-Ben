19a88334d944538d3fa0d5766bd76d053ac1b62a
Jenemina <jeneby.lopez.cisternas@gmail.com>
2024-06-30 23:12:44 -0400

    JENEBY = cuerpo de base de datos y complementos aun no terminados.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tienda'
+    'tienda',
+    'crispy_forms',
+    'crispy_bootstrap5'
+
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- tienda/views.py
+++ tienda/views.py
@@ -1,21 +1,43 @@
 from django.shortcuts import render
+from .models import Vinilo
+from .models import Jazz
+from .models import Clasica
+from .models import Rock
+
 
 # Create your views here.
+
 def home(request):
     return render(request, 'tienda/inicio.html')
+
 def carrito(request):
     return render(request, 'tienda/vCarrito.html')
+
 def clasica(request):
+
     return render(request, 'tienda/vclasica.html')
+
 def contacto(request):
     return render(request, 'tienda/vContacto.html')
+
 def inisesion(request):
     return render(request, 'tienda/vInicioSesion.html')
+
 def jazz(request):
-    return render(request, 'tienda/vjazz.html')
+
+    jazzs = Jazz.objects.all()
+    data = {"jazzs" : jazzs}
+
+    return render(request, 'tienda/vjazz.html', data)
+
 def nosotros(request):
     return render(request, 'tienda/vNosotros.html')
+    
 def registro(request):
     return render(request, 'tienda/vRegistro.html')
+
 def rock(request):
     return render(request, 'tienda/vrock.html')
+
+def agregar_producto(request):
+    return render(request, 'tienda/vinilo/agregar.html')
